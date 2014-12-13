f6bd1c49fbc04a4efd8cea458d9037f2f0c1f3b7
Juan Manuel Torres <kinojman@yahoo.com>
2014-12-13 20:56:51 +0000

    Addign new inventory interface.

---
+++ src/Inventory/InventoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the SDPHP php-micrork Package.
+ * For the full copyright and license information, 
+ * please view the LICENSE file that was distributed 
+ * with this source code.
+ */
+
+namespace SDPHP\PHPMicrork\Inventory;
+
+
+/**
+ * InventoryInterface - Description. 
+ *
+ * @author Juan Manuel Torres <kinojman@gmail.com>
+ */
+interface InventoryInterface
+{
+    public function setItem(ItemInterface $item);
+    
+    public function getItem($itmeName);
+
+    public function hasItem($itemName);
+    
+    public function removeItem($itemName);
+}
